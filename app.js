af6a836f11193db7446cbabf3d44d90c6f3245b0
Ayush Kumar Guta <akgmac@Ayushs-MacBook-Pro.local>
2022-02-27 23:13:33 +0530

    Initial Merge

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var api = require('./sortAPI');
+var cors = require('cors')
 var app = express();
+
+
 const port=process.env.PORT || 4000
 //enable body parse
 app.configure(function(){
-	app.use(express.bodyParser());
+	app.use(express.bodyParser(),cors());
 });
 
 //routes
